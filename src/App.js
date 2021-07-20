6bba917c062e21dd94659655cb6eb3ef72556f92
Andres Gardiol <andresgardiol@gmail.com>
2021-07-20 20:59:01 -0300

    Counter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button';
+import {Col, Row} from "react-bootstrap";
+
+
+function ButtonWrapper(props) {
+    return (
+        <Button variant={props.variant} onClick={props.onClickEvent}>
+            {props.children}
+        </Button>
+    )
+}
+
+function CounterDisplay(props) {
+    return (
+        <div>
+            {props.counter}
+        </div>
+    )
+}
+
+function Counter() {
+    const [counter, setCounter] = useState(0);
+    const handleAddEvent = () => {
+        setCounter(counter + 1);
+    }
+    const handleResetEvent = () => {
+        setCounter(0);
+    }
+    return (
+        <Row className="justify-content-md-center">
+            <Col xs="auto">
+                <Row>
+                    <Col xs="auto">Times clicked</Col>
+                    <Col xs="auto"><CounterDisplay counter={counter}/></Col>
+                </Row>
+                <Row>
+                    <Col xs={8}><ButtonWrapper variant="primary" onClickEvent={handleAddEvent}>Click me</ButtonWrapper></Col>
+                    <Col xs={4}><ButtonWrapper variant="warning"
+                                               onClickEvent={handleResetEvent}>Reset</ButtonWrapper></Col>
+                </Row>
+            </Col>
+        </Row>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Counter></Counter>
+    );
 }
 
 export default App;
