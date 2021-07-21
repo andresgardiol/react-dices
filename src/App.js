abe0bbadde95b50cb2725d357513253cec2de66a
Andres Gardiol <andresgardiol@gmail.com>
2021-07-20 21:50:57 -0300

    Create a window size hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
+
+function useWindowSize() {
+    let [windowWidth, setWindowWidth] = useState([window.innerWidth, window.innerHeight]);
+
+    function updateWindowWidth() {
+        return setWindowWidth([window.innerWidth, window.innerHeight]);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWindowWidth);
+        return () => window.removeEventListener('resize', updateWindowWidth);
+    }, []);
+    return windowWidth;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [width, height] = useWindowSize();
+
+    return (
+        <div className="App">
+            <h1>Window width is: {width}px</h1>
+            <h1>Window height is: {height}px</h1>
+        </div>
+    );
 }
 
 export default App;
